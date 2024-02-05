264b979a47cc1fa62d03fe722a27b1fe1470ba15
norven.caracas <norven.caracas@gmail.com>
2024-02-05 21:09:14 +0800

    Add authentication code

---
+++ utils/auth.js
@@ -0,0 +1,71 @@
+import jwt from "jsonwebtoken";
+
+import secret from "EcommerceAPI";
+//const secret = "EcommerceAPI";
+
+
+/* ===== Token Creation ===== */
+module.exports.createAccessToken = (user) => {
+	const data = {
+		id: user._id,
+		email: user.email,
+		isAdmin: user.isAdmin
+	};
+	return jwt.sign(data, secret, {});
+}
+
+/* ===== Token Verification ===== */
+module.exports.verify = (req,res, next) => {
+	console.log(req.headers.authorization);
+
+	let token = req.headers.authorization;
+
+	if(typeof token === "undefined"){
+		return res.send({ auth: "Failed. No token"})
+	}
+	else{
+		console.log(token);
+		token = token.slice(7, token.length);
+		console.log(token); 
+
+        // Token decryption
+		jwt.verify(token, secret, function(err, decodedToken){
+			if(err){
+				return res.send(
+				{
+					auth: "Failed",
+					message: err.message
+				});
+			}
+			else{
+				console.log("result from verify method: ");
+				console.log(decodedToken);
+
+				req.user = decodedToken;
+				next();
+			}
+		})
+	}
+}
+
+/* ===== Admin Verification ===== */
+module.exports.verifyAdmin = (req, res, next) =>{
+	if(req.user.isAdmin){
+		next();
+	}
+	else{
+		return res.status(403).send({
+			auth: "Failed",
+			message: "Action Forbidden"
+		})
+	}
+}
+
+/* ===== Check if Logged in ===== */
+// module.exports.isLoggedIn = (req, res, next) => {
+// 	if(req.user){
+// 		next();
+// 	} else {
+// 		res.sendStatus(401);
+// 	}
+// }
