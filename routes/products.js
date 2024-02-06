0b9632846cabc79015d2ccde57f42a2fe326b82a
norven.caracas <norven.caracas@gmail.com>
2024-02-06 19:15:08 +0800

    Added product routes code

---
+++ routes/products.js
@@ -0,0 +1,27 @@
+/* ===== Dependencies and Modules ===== */
+const express = require("express");
+
+const productController = require("../controllers/product.js");
+const { verify, verifyAdmin } = require("../utils/auth.js");
+
+/* ===== Routing Component ===== */
+const router = express.Router();
+
+
+/* ===== User Routes ===== */
+router.post("/", verify, verifyAdmin, productController.createProduct);
+
+// router.get("/all", verify, productController.getAllProducts);
+
+// router.get("/active", verify, productController.getAllActive);
+
+// router.get("/:productId", verify, productController.getProduct);
+
+// router.put("/:productId", verify, verifyAdmin, productController.updateProduct);
+
+router.put("/archive/:productId", verify, verifyAdmin, productController.archiveProduct);
+
+router.put("/activate/:productId", verify, verifyAdmin, productController.activateProduct);
+
+
+module.exports = router;
