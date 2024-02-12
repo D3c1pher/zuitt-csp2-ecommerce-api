dbd5d6d731cd46b6ec9715525e68ccbc878c43a7
norven.caracas <norven.caracas@gmail.com>
2024-02-12 20:39:24 +0800

    Added confirmation and logout features

--- controllers/user.js
+++ controllers/user.js
@@ -1,16 +1,15 @@
 /* ===== Dependencies and Modules ===== */
-const bcrypt = require("bcrypt");
-/* ===== Models ===== */
+const bcrypt = require('bcrypt');
+const crypto = require('crypto');
 const User = require("../models/User.js");
-/* ===== Middlewares ===== */
+const Token = require("../models/Token.js");
 const auth = require("../middlewares/authentication.js");
 const { createError } = require("../middlewares/error.js");
-/* ===== Validations ===== */
-const { 
-    validateInputs, 
-    validateEmail, 
-    validateMobileNo, 
-    validatePassword 
+const {
+    validateInputs,
+    validateEmail,
+    validateMobileNo,
+    validatePassword
 } = require("../middlewares/validations.js");
 
 
@@ -21,21 +20,22 @@ module.exports.registerUser = async (req, res, next) => {
         const { firstName, lastName, username, email, mobileNo, password } = req.body;
 
         if (!validateInputs(firstName, lastName, username, email, mobileNo, password))
-            throw createError(400, "All fields are required");
+            throw createError(400, "All fields are required!");
 
         if (!validateEmail(email))
-            throw createError(400, "Email is invalid");
+            throw createError(400, "Email is invalid!");
 
         if (!validateMobileNo(mobileNo))
-            return next(createError(400, "Mobile Number is invalid"));
+            throw createError(400, "Mobile Number is invalid!");
 
         if (!validatePassword(password))
-            return next(createError(400, "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character"));
+            throw createError(400, "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.");
 
         const existingUser = await User.findOne({ $or: [{ email }, { username }] });
+        
         if (existingUser) {
             const field = existingUser.email === email ? "Email" : "Username";
-            throw createError(409, `${field} already exists`);
+            throw createError(409, `${field} already exists!`);
         }
 
         const hashedPassword = await bcrypt.hash(password, bcrypt.genSaltSync(10));
@@ -48,12 +48,24 @@ module.exports.registerUser = async (req, res, next) => {
             mobileNo,
             password: hashedPassword
         });
+        await newUser.save();
 
-        const registeredUser = await newUser.save();
+        const token = new Token({ 
+            userId: newUser._id, 
+            token: crypto.randomBytes(16).toString('hex') 
+        });
+        await token.save();
+        
+        const mailOptions = {
+            from: 'no-reply@example.com',
+            to: newUser.email,
+            subject: 'Account Verification Link',
+            text: `Hello ${req.body.firstName}, Please verify your account by clicking the link: http://${req.headers.host}/users/confirmation/${newUser.email}/${token.token} Thank You!`
+        };
 
-        return res.status(201).send({
-            message: "User is registered successfully",
-            registeredUser: registeredUser
+        return res.status(200).send({
+            message: `A verification email has been sent to ${newUser.email}. It will expire after one day. If you don't receive the verification email, click on resend token.`,
+            mail: mailOptions
         });
     } catch (err) {
         console.error("Error in user registration: ", err);
@@ -61,22 +73,98 @@ module.exports.registerUser = async (req, res, next) => {
     }
 };
 
+module.exports.confirmEmail = async (req, res, next) => {
+    try {
+        const token = await Token.findOne({ token: req.params.token });
+        
+        if (!token)
+            throw createError(400, "Your verification link may have expired. Please click on resend to verify your Email.");
+
+        const user = await User.findOne({ _id: token.userId, email: req.params.email });
+
+        if (!user)
+            throw createError(401, "We were unable to find a user for this verification. Please sign up!");
+
+        if (user.isVerified)
+            return res.status(200).send({message: "User has already been verified. Please login."});
+
+        user.isVerified = true;
+        await user.save();
+
+        await Token.findByIdAndDelete(token._id);
+
+        const registeredUser = await User.findOne({ email: req.params.email });
+
+        return res.status(200).send({
+            message: "Your account has been successfully verified.",
+            registeredUser: registeredUser
+    });
+    } catch (err) {
+        console.error("Error in email confirmation: ", err);
+        return next(err);
+    }
+};
+
+module.exports.resendLink = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ email: req.body.email });
+
+        if (!user)
+            throw createError(401, "We were unable to find a user with that email. Make sure your Email is correct!");
+
+        if (user.isVerified)
+            return res.status(200).send('This account has been already verified. Please log in.');
+
+        const token = new Token({ userId: user._id, token: crypto.randomBytes(16).toString('hex') });
+        await token.save();
+
+        const mailOptions = {
+            from: 'no-reply@example.com',
+            to: user.email,
+            subject: 'Account Verification Link',
+            text: `Hello ${user.firstName}, Please verify your account by clicking the link: http://${req.headers.host}/users/confirmation/${user.email}/${token.token} Thank You!`
+        };
+
+        return res.status(200).send({
+            message: `A verification email has been sent to ${user.email}. It will expire after one day. If you don't receive the verification email, click on resend token.`,
+            mail: mailOptions
+        });
+    } catch (err) {
+        console.error("Error in resending verification link: ", err);
+        next(err);
+    }
+};
+
+/* ========== ========== */
+
 module.exports.loginUser = async (req, res, next) => {
     try {
-		console.log(req.body);
+        if (req.session.userId) {
+            req.session.destroy();
+        }
+
         const { email, password } = req.body;
 
         if (!validateInputs(email, password))
-            throw createError(400, "Input your email and password");
+            throw createError(400, "Input your email and password!");
 
 		if (!validateEmail(email))
-            throw createError(400, "Email is invalid");
+            throw createError(400, "Email is invalid!");
 
         const user = await User.findOne({ email });
-        if (!user) throw createError(404, "User does not exist");
+
+        if (!user) 
+            throw createError(404, `The email address ${req.body.email} is not associated with any account. Please check and try again.`);
 
         const isPasswordCorrect = await bcrypt.compare(password, user.password);
-        if (!isPasswordCorrect) throw createError(401, "Password is invalid");
+
+        if (!isPasswordCorrect)
+            throw createError(401, "Password is incorrect!");
+
+        if (!user.isVerified)
+            throw createError(401, "Your account has not been verified. Please click on resend to verify your Email.");
+
+        req.session.userId = user._id;
 
         const accessToken = auth.createAccessToken(user);
         return res.status(200).send({ 
@@ -89,14 +177,26 @@ module.exports.loginUser = async (req, res, next) => {
     }
 };
 
+module.exports.logoutUser = async (req, res, next) => {
+    try {
+        req.session.destroy();
+
+        return res.status(200).json({ message: "User logged out successfully." });
+    } catch (err) {
+        console.error("Error in logging out: ", err);
+        return next(err);
+    }
+};
+
+/* ========== ========== */
+
 module.exports.getUserDetails = async (req, res, next) => {
 	try {
         const userId = req.user.id;
         const user = await User.findById(userId);
 
-        if (!user) {
-			throw createError(404, "User not found");
-        }
+        if (!user)
+			throw createError(404, "User does not exist.");
 
         user.password = undefined;
 
@@ -107,48 +207,87 @@ module.exports.getUserDetails = async (req, res, next) => {
     }
 };
 
-module.exports.updatePassword = async (req, res) => {
+/* ========== ========== */
+
+module.exports.updatePassword = async (req, res, next) => {
 	try {
-		console.log(req.body);
-		const { newPassword } = req.body;
+        const { newPassword, confirmPassword } = req.body;
         const { id } = req.user;
 
-        if (!newPassword)
-            throw createError(400, "New password is required");
+        if (!newPassword || !confirmPassword)
+            throw createError(400, "New password and confirm password are required.");
+
+        if (newPassword !== confirmPassword)
+            throw createError(400, "New password and confirm password must match.");
 
         if (!validatePassword(newPassword))
-            throw createError(400, "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character");
+            throw createError(400, "Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.");
 
-        if (!id) throw createError(401, "Unauthorized Access");
+        const token = new Token({ userId: id, token: crypto.randomBytes(16).toString('hex') });
+        await token.save();
 
-		const hashedPassword = await bcrypt.hash(newPassword, bcrypt.genSaltSync(10));
+        await User.findByIdAndUpdate(id, { newPassword, passwordResetToken: token.token });
 
-        await User.findByIdAndUpdate(id, { password: hashedPassword });
+        const mailOptions = {
+            from: 'no-reply@example.com',
+            to: req.user.email,
+            subject: 'Password Change Confirmation Link',
+            text: `Hello ${req.user.firstName}, Please confirm password change by clicking the link: http://${req.headers.host}/users/confirm-password-change/${token.token} Thank You!`
+        };
 
-		res.status(200).json({ message: "Password reset successfully" });
+        return res.status(200).send({
+            message: `Password reset request received. A confirmation email has been sent to ${req.user.email}. It will expire after one day. If you didn't receive a confirmation email, click on resend token.`,
+            mail: mailOptions
+        });
 	} catch (error) {
 		console.error("Error in updating password: ", err);
 		return next(err);
 	}
 };
 
+module.exports.confirmPasswordChange = async (req, res, next) => {
+    try {
+        const token = await Token.findOne({ token: req.params.token });
+        
+        if (!token)
+            throw createError(400, "Your confirmation link may have expired. Please click on resend to confirm password change.");
+
+        const user = await User.findById(token.userId);
+
+        if (!user)
+            throw createError(400, "Invalid or expired token.");
+
+        const hashedPassword = await bcrypt.hash(user.newPassword, bcrypt.genSaltSync(10));
+        user.password = hashedPassword;
+        user.newPassword = undefined;
+        user.passwordResetToken = undefined;
+        await user.save();
+
+        await Token.findByIdAndDelete(token._id);
+
+        res.status(200).send({ message: "Password changed successfully." });
+    } catch (err) {
+        console.error("Error in confirming password change: ", err);
+        next(err);
+    }
+};
+
+/* ========== ========== */
+
 module.exports.updateUserToAdmin = async (req, res, next) => {
     try {
-        console.log(req.params.userId);
-
         if (!req.params.userId.match(/^[0-9a-fA-F]{24}$/))
-            throw createError(400, "Invalid user ID");
+            throw createError(400, "Invalid user ID!");
 
         const updateUserToAdmin = {
             isAdmin: true
         };
-
         const updatedUser = await User.findByIdAndUpdate(req.params.userId, updateUserToAdmin);
 
         if (!updatedUser)
-            throw createError(404, "User not found");
+            throw createError(404, "User does not exist.");
 
-		res.status(200).json({ message: "User updated to admin successfully" });
+		res.status(200).json({ message: "User updated to admin successfully." });
     } catch (err) {
         console.error("Error in updating user to admin: ", err);
         return next(err);
