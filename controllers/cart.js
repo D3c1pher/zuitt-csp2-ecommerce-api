3282a1271c1fef709f0d124c73c94f0b18b8f730
norven.caracas <norven.caracas@gmail.com>
2024-02-13 15:15:35 +0800

    Modified cart validations

--- controllers/cart.js
+++ controllers/cart.js
@@ -19,10 +19,10 @@ const {
 module.exports.getUserCart = async (req, res, next) => {
     try {
         const userCart = await findOrCreateUserCartLean(req.user.id);
-        if (!userCart) throw createError(404, "User's cart not found");
+        if (!userCart) throw createError(404, "User's cart not found.");
 
         if (userCart.cartItems.length === 0)
-            return res.status(200).json({ message: "User's cart is empty" });
+            return res.status(200).json({ message: "User's cart is empty." });
         
         const formattedCart = formatCart(userCart);
         return res.status(200).json({ cart: formattedCart });
@@ -36,12 +36,12 @@ module.exports.addToCart = async (req, res, next) => {
     try {
         const { productId, quantity } = req.body;
         if (!validateInputs(productId, quantity) || quantity <= 0)
-            throw createError(400, "Invalid input data. Please provide a valid productId and a positive quantity");
+            throw createError(400, "Invalid input data. Please provide a valid productId and a positive quantity.");
         
         let cart = await findOrCreateUserCart(req.user.id);
         const product = await Product.findById(productId).lean();
 
-        if (!product) throw createError(404, "Product does not exist");
+        if (!product) throw createError(404, "Product does not exist.");
 
         const subtotal = product.price * quantity;
         updateCartWithItem(cart, productId, quantity, subtotal);
@@ -59,10 +59,15 @@ module.exports.updateProductQuantity = async (req, res, next) => {
     try {
         const { productId, quantity } = req.body;
         if (!validateInputs(productId, quantity) || quantity <= 0)
-            throw createError(400, "Invalid input data. Please provide a valid productId and a positive quantity");
+            throw createError(400, "Invalid input data. Please provide a valid productId and a positive quantity.");
 
         const cart = await findOrCreateUserCart(req.user.id);
-        if (!cart) throw createError(404, "Cart does not exist");
+        if (!cart) throw createError(404, "Cart does not exist.");
+
+        const productIndex = cart.cartItems.findIndex(item => item.productId.equals(productId));
+
+        if (productIndex === -1) 
+            throw createError(404, "Product not found in cart.");
 
         await updateCartItemQuantity(cart, productId, quantity);
 
@@ -79,7 +84,7 @@ module.exports.updateProductQuantity = async (req, res, next) => {
 module.exports.removeProductFromCart = async (req, res, next) => {
     try {
         if (!req.params.productId.match(/^[0-9a-fA-F]{24}$/))
-            throw createError(400, "Invalid product ID");
+            throw createError(400, "Invalid product ID!");
         const productId = req.params.productId;
 
         const userId = req.user.id;
@@ -88,7 +93,7 @@ module.exports.removeProductFromCart = async (req, res, next) => {
         const productIndex = cart.cartItems.findIndex(item => item.productId.equals(productId));
 
         if (productIndex === -1) 
-            throw createError(404, "Product not found in cart");
+            throw createError(404, "Product not found in cart.");
 
         cart.cartItems.splice(productIndex, 1);
         cart.totalPrice = calculateTotalPrice(cart.cartItems);
@@ -98,8 +103,11 @@ module.exports.removeProductFromCart = async (req, res, next) => {
         const updatedCart = await Cart.findById(cart._id).lean();
         const formattedCart = formatCart(updatedCart);
 
+        if (cart.cartItems.length === 0)
+            await Cart.deleteMany({ userId: userId });
+
         return res.status(200).json({
-            message: "Product removed from cart successfully", 
+            message: "Product removed from cart successfully.", 
             updatedCart: formattedCart });
     } catch (err) {
         console.error("Error in removing the product: ", err);
@@ -111,10 +119,10 @@ module.exports.clearCartItems = async (req, res, next) => {
     try {
         const { deletedCount } = await Cart.deleteMany({ userId: req.user.id });
         if (deletedCount === 0) {
-            throw createError(404, "No Items Available");
+            throw createError(404, "No Items Available.");
         }
 
-        return res.status(200).json({ message: "All items cleared from the cart" });
+        return res.status(200).json({ message: "All items cleared from the cart." });
     } catch (err) {
         console.error("Error in clearing the cart items: ", err);
         return next(err);
